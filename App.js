c962746ac01dc1c164f016cf1154ed0bb914f14e
Johnny Park <110794898+jpark42@users.noreply.github.com>
2023-05-10 23:28:14 -0400

    Initialize Firestone into App

--- App.js
+++ App.js
@@ -4,6 +4,9 @@ import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
 // import the screens
 import Start from "./components/Start";
 import Chat from "./components/Chat";
@@ -13,6 +16,23 @@ const Stack = createNativeStackNavigator();
 
 // App's main component that renders the UI
 const App = () => {
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAPqVWyNKHmnKAAfVKURr3yDlhd4kE5_SU",
+    authDomain: "chatmate-a350b.firebaseapp.com",
+    projectId: "chatmate-a350b",
+    storageBucket: "chatmate-a350b.appspot.com",
+    messagingSenderId: "591583362220",
+    appId: "1:591583362220:web:12c74d2b444ce470aa7309",
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  // Initialize Cloud Firestore and get a reference to the service
+  // When using this in other components, you can read from, and write into, your database via your app
+  const db = getFirestore(app);
+
   return (
     /*Reponsible for managing your app state and linking your top-level navigator to the app*/
     <NavigationContainer>
